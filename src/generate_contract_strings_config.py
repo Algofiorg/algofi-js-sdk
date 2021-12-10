7048550de256fdbc9f5b43ed734fd07ce84f20c6
Paul-Colella <paul.colella.a@gmail.com>
2021-12-10 17:18:16 +0000

    update strings

--- src/generate_contract_strings_config.py
+++ src/generate_contract_strings_config.py
@@ -5,14 +5,12 @@ from globals import *
 if __name__ == "__main__":
     f = open("contractStrings.ts", "w")
     
-    market_strings = algofi_market_strings()
     f.write("export const marketStrings = {\n")
-    for key, value in market_strings.__dict__.items():
+    for key, value in algofi_market_strings.__dict__.items():
         f.write("    {} : \"{}\",\n".format(key, value))
     f.write("}\n")
     
-    manager_strings = algofi_manager_strings()
     f.write("export const managerStrings = {\n")
-    for key, value in manager_strings.__dict__.items():
+    for key, value in algofi_manager_strings.__dict__.items():
         f.write("    {} : \"{}\",\n".format(key, value))
     f.write("}\n")
