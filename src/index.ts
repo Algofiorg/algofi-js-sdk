bb064b5c5f9df6808db7ca63d843934a0a5b4466
Midhun Dandamudi <midhunsezhi@gmail.com>
2021-12-28 18:35:34 +0530

    remove usdcstblStaker

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,8 @@ import * as v0 from "./v0";
 import * as testnet from "./testnet";
 import * as testnetStaker from "./testnetStaker";
 import * as mainnetStaker from "./mainnetStaker";
-import * as usdcstblStaker from "./usdcstblStaker";
 
 // export current main version (v0) as top-level
 export * from "./v0";
 
-export {v0, testnet, testnetStaker, mainnetStaker, usdcstblStaker} 
+export {v0, testnet, testnetStaker, mainnetStaker} 
