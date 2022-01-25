0bfa8da3ba4a0826a44c500101981fee42f9c985
Armstrong Park <armstrongpark@Armstrongs-MacBook-Pro.local>
2022-01-25 10:48:56 -0500

    clients first draft r

--- src/v1/client.ts
+++ src/v1/client.ts
@@ -613,25 +613,28 @@ export class Client {
 /*
 ************JAVASCRIPT************
 
-INDEXER
+JAVASCRIPT INDEXER
 
 const indexer_token = "";
 const indexer_server = "http://localhost";
 const indexer_port = 8980;
 
-const indexerClient = new algosdk.Indexer(indexer_token, indexer_server, indexer_port);
+const indexerClient = new algosdk.Indexer("", http://localhost", 8980);
 
 
-CLIENT
+JAVASCRIPT CLIENT
 
 const algodToken = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
         const algodServer = 'http://localhost';
         const algodPort = 4001;
         let algodClient = new algosdk.Algodv2(algodToken, algodServer, algodPort);
 
+
+
+
 ************PYTHON************
 
-INDEXER
+PYTHON INDEXER
 
 import json
 # requires Python SDK version 1.3 or higher
@@ -641,7 +644,7 @@ from algosdk.v2client import indexer
 myindexer = indexer.IndexerClient(indexer_token="", indexer_address="http://localhost:8980")
 
 
-CLIENT
+PYTHON CLIENT
 
 algod_address = "http://localhost:4001"
     algod_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
@@ -650,28 +653,29 @@ algod_address = "http://localhost:4001"
 */
 
 export class AlgofiTestnetClient extends Client{
-  constructor(algodClient : Algodv2, indexerClient : Indexer,  userAddress : string){
-    let historicalIndexerClient = new Indexer("", "https://indexer.testnet.algoexplorerapi.io/", 8980, {"User-Agent":"algosdk"})
+  constructor(algodClient : Algodv2, indexerClient : Indexer = undefined,  userAddress : string = undefined){
+    let historicalIndexerClient = new Indexer("", "https://indexer.testnet.algoexplorerapi.io/", 8980, {"User-Agent":"algosdk"}) 
     if (algodClient === undefined) {
       let algodClient = new Algodv2("", "https://api.testnet.algoexplorer.io", 4001, {"User-Agent": "algosdk"})
-    }
-    if (indexerClient == undefined) {
-      let indexerClient = new Indexer("", "https://algoindexer.testnet.algoexplorerapi.io", 8980, {"User-Agent": "algosdk"})
+    }   
+    if (indexerClient === undefined) {
+      let indexerClient = new Indexer("", "https://algoindexer.testnet.algoexplorerapi.io", 8980, {"User-Agent":"algosdk"})
     }
     super(algodClient, indexerClient, historicalIndexerClient, userAddress, "testnet")
-    }
+}
 }
 
-export class AlgofiMainnetClient extends Client{
-  constructor(algodClient : Algodv2, indexerClient : Indexer,  userAddress : string){
-    let historicalIndexerClient = new Indexer("",  "https://indexer.algoexplorerapi.io/", 8980, {"User-Agent":"algosdk"})
+
+export class AlgofiMainnetClient extends Client {
+  constructor(algodClient : Algodv2, indexerClient : Indexer = undefined,  userAddress : string = undefined){
+    let historicalIndexerClient = new Indexer("", "https://indexer.algoexplorerapi.io/", 8980, {"User-Agent":"algosdk"}) 
     if (algodClient === undefined) {
-      let algodClient = new Algodv2("", 'https://algoexplorerapi.io', 4001, {"User-Agent": "algosdk"})
-    }
-    if (indexerClient == undefined) {
-      let indexerClient = new Indexer("", "https://algoindexer.algoexplorerapi.io", 8980, {"User-Agent": "algosdk"})
+      let algodClient = new Algodv2("", "https://algoexplorerapi.io", 4001, {"User-Agent": "algosdk"})
+    }   
+    if (indexerClient === undefined) {
+      let indexerClient = new Indexer("", "https://algoindexer.algoexplorerapi.io", 8980, {"User-Agent":"algosdk"})
     }
     super(algodClient, indexerClient, historicalIndexerClient, userAddress, "mainnet")
-    }
+}
 }
 
