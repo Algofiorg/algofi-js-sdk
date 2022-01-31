7b48ed5bb935c0340a7405532d7c622b5934223f
Armstrong Park <armstrongpark@Armstrongs-MacBook-Pro.local>
2022-01-31 16:50:39 -0500

    liquidate doc

--- src/v1/liquidate.ts
+++ src/v1/liquidate.ts
@@ -11,6 +11,30 @@ import { getInitTxns } from "./prepend"
 
 const enc = new TextEncoder()
 
+/**
+ * Returns a transaction group object representing a liquidation group
+ * transaction against the algofi protocol. The sender (liquidator) repays up to
+ * 50% of the liquidatee's outstanding borrow and takes collateral of the liquidatee
+ * at a premium defined by the market. The liquidator first sends borrow assets to the
+ * account address of the borrow market. Then, the account of the collateral market is authorized
+ * to credit the liquidator with a greater value of the liquidatee's collateral. The liquidator can
+ * then remove collateral to underlying to convert the collateral to assets.
+ *
+ * @param sender -account address for the sender
+ * @param suggestedParams - suggested transaction params
+ * @param storageAccount - storage account address for sender (liquidator)
+ * @param liquidateeStorageAccount - storage account address for liquidatee
+ * @param amount - amount of borrow the liquidator repays
+ * @param managerAppId - id of the manager application
+ * @param borrowMarketAppId - id of the borrow market application
+ * @param borrowMarketAddress - account address of the borrow market
+ * @param collateralMarketAppId - id of the collateral market application
+ * @param supportedMarketAppIds - list of supported market application ids
+ * @param supportedOracleAppIds - list of supported oracle application ids
+ * @param collateralBankAssetId - id of the collateral bank asset
+ * @param borrowAssetId - id of the borrow asset, defaults to none (algo)
+ * @returns transaction group object representing a liquidate group transaction
+ */
 export function prepareLiquidateTransactions(
   sender: string,
   suggestedParams: SuggestedParams,
@@ -26,7 +50,7 @@ export function prepareLiquidateTransactions(
   collateralBankAssetId: number,
   borrowAssetId: number = null
 ): TransactionGroup {
-  let prefixTransactions = getInitTxns(
+  const prefixTransactions = getInitTxns(
     Transactions.LIQUIDATE,
     sender,
     suggestedParams,
@@ -35,7 +59,7 @@ export function prepareLiquidateTransactions(
     supportedOracleAppIds,
     liquidateeStorageAccount
   )
-  let txn0 = makeApplicationNoOpTxn(
+  const txn0 = makeApplicationNoOpTxn(
     sender,
     suggestedParams,
     managerAppId,
@@ -44,7 +68,7 @@ export function prepareLiquidateTransactions(
     supportedMarketAppIds
   )
 
-  let txn1 = makeApplicationNoOpTxn(
+  const txn1 = makeApplicationNoOpTxn(
     sender,
     suggestedParams,
     borrowMarketAppId,
@@ -69,7 +93,7 @@ export function prepareLiquidateTransactions(
     txn2 = makePaymentTxnWithSuggestedParams(sender, borrowMarketAddress, amount, undefined, undefined, suggestedParams)
   }
 
-  let txn3 = makeApplicationNoOpTxn(
+  const txn3 = makeApplicationNoOpTxn(
     sender,
     suggestedParams,
     collateralMarketAppId,
