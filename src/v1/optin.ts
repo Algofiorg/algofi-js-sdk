442bb7713c7ac9bd0b880e833974a1bbfee5e2fc
Armstrong Park <armstrongpark@Armstrongs-MacBook-Pro.local>
2022-01-31 17:26:20 -0500

    opt in doc

--- src/v1/optin.ts
+++ src/v1/optin.ts
@@ -8,6 +8,19 @@ import {
 import { TransactionGroup, getRandomInt, intToBytes } from "./utils"
 const OPT_IN_MIN_BALANCE = 3.5695
 
+/**
+ * Returns a transactiong roup object representing a manager opt in
+ * group transaction. The sender and storage account opt in to the manager application
+ * and the storage account is rekeyed to the manager account address, rendering it
+ * unable to be transacted against by the sender and therefore immutable.
+ *
+ * @param managerAppId - id of the manager application
+ * @param getMaxAtomicOptInMarketAppIds - max opt in market app ids
+ * @param sender - account address for the sender
+ * @param storageAddress - address of the storage account
+ * @param suggestedParams - suggested transaction params
+ * @returns transaction group object representing a managet opt in group transaction
+ */
 export function prepareManagerAppOptinTransactions(
   managerAppId: number,
   getMaxAtomicOptInMarketAppIds: number[],
@@ -15,7 +28,7 @@ export function prepareManagerAppOptinTransactions(
   storageAddress: string,
   suggestedParams: SuggestedParams
 ): TransactionGroup {
-  let txnPayment = makePaymentTxnWithSuggestedParams(
+  const txnPayment = makePaymentTxnWithSuggestedParams(
     sender,
     storageAddress,
     Math.floor(OPT_IN_MIN_BALANCE * 1e6),
@@ -24,15 +37,15 @@ export function prepareManagerAppOptinTransactions(
     suggestedParams
   )
 
-  let marketOptinTransactions = []
-  for (let marketAppId of getMaxAtomicOptInMarketAppIds) {
+  const marketOptinTransactions = []
+  for (const marketAppId of getMaxAtomicOptInMarketAppIds) {
     marketOptinTransactions.push(makeApplicationOptInTxn(sender, suggestedParams, marketAppId))
   }
 
-  let txnUserOptinManager = makeApplicationOptInTxn(sender, suggestedParams, managerAppId)
+  const txnUserOptinManager = makeApplicationOptInTxn(sender, suggestedParams, managerAppId)
 
-  let appAddress = getApplicationAddress(managerAppId)
-  let txnStorageOptinManager = makeApplicationOptInTxn(
+  const appAddress = getApplicationAddress(managerAppId)
+  const txnStorageOptinManager = makeApplicationOptInTxn(
     storageAddress,
     suggestedParams,
     managerAppId,
@@ -47,6 +60,15 @@ export function prepareManagerAppOptinTransactions(
   return new TransactionGroup([txnPayment, ...marketOptinTransactions, txnUserOptinManager, txnStorageOptinManager])
 }
 
+/**
+ * Returns a transaction group object representing a market opt in
+ * group transaction.
+ *
+ * @param marketAppId -id of the market application
+ * @param sender - account address for the sender
+ * @param suggestedParams - suggested transaction params
+ * @returns transaction group object representing a market opt in group transaction
+ */
 export function prepareMarketAppOptinTransactions(
   marketAppId: number,
   sender: string,
@@ -57,11 +79,29 @@ export function prepareMarketAppOptinTransactions(
   ])
 }
 
+/**
+ * Returns a transaction group object representing an asset opt in
+ * group transaction.
+ *
+ * @param assetId - id of the asset to opt into
+ * @param sender - account address for the sender
+ * @param suggestedParams - suggested transaction params
+ * @returns transaction group object representing an asset opt in group transaction
+ */
 export function prepareAssetOptinTransactions(
   assetId: number,
   sender: string,
   suggestedParams: SuggestedParams
 ): TransactionGroup {
-  let txn = makeAssetTransferTxnWithSuggestedParams(sender, sender, undefined, undefined, 0, undefined, assetId, suggestedParams)
+  const txn = makeAssetTransferTxnWithSuggestedParams(
+    sender,
+    sender,
+    undefined,
+    undefined,
+    0,
+    undefined,
+    assetId,
+    suggestedParams
+  )
   return new TransactionGroup([txn])
 }
