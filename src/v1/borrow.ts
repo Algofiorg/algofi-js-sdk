29052f732a9572dee0d1675cc5f86cc92c2eb4c3
Armstrong Park <armstrongpark@Armstrongs-MacBook-Pro.local>
2022-01-31 16:40:19 -0500

    borrow documentation initial

--- src/v1/borrow.ts
+++ src/v1/borrow.ts
@@ -5,6 +5,22 @@ import { getInitTxns } from "./prepend"
 
 const enc = new TextEncoder()
 
+/**
+ * Returns a :class:`TransactionGroup` object representing an borrow group
+ * transaction against the algofi protocol. Protocol sends requested borrow asset
+ * to the sender account provided sufficient collateral has been posted
+ *
+ * @param sender - account address for the sender
+ * @param suggestedParams - suggested transaction params
+ * @param storageAccount - storage address for the sender
+ * @param amount - amount of asset to borrow
+ * @param assetId - asset id of the asset to be borrowed
+ * @param managerAppId - id of the manager application
+ * @param marketAppId - id of the market application for the borrow asset
+ * @param supportedMarketAppIds - list of supported market application ids
+ * @param supportedOracleAppIds - list of supported oracle applicaiton ids
+ * @returns transaction group representing a borrow group transaction
+ */
 export function prepareBorrowTransactions(
   sender: string,
   suggestedParams: SuggestedParams,
