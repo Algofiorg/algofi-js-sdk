5776ea035fea423589f7134bb058986d9f96ae64
Armstrong Park <armstrongpark@Armstrongs-MacBook-Pro.local>
2022-01-31 16:44:39 -0500

    claim rewards doc

--- src/v1/claimRewards.ts
+++ src/v1/claimRewards.ts
@@ -5,6 +5,21 @@ import { getInitTxns } from "./prepend"
 
 const enc = new TextEncoder()
 
+/**
+ * Returns a :class:`TransactionGroup` object representing a claim rewards
+ * underlying group transaction against the algofi protocol. The sender requests
+ * to claim rewards from the manager acount. If not, the account sends
+ * back the user the amount of asset underlying their posted collateral.
+ *
+ * @param sender - account address for the sender
+ * @param suggestedParams - suggested transaction params
+ * @param storageAccount - storage account address for sender
+ * @param managerAppId - id of the manager application
+ * @param supportedMarketAppIds - list of supported market application ids
+ * @param supportedOracleAppIds - list of supported oracle application ids
+ * @param foreignAssets - list of rewards assets in the staking contract
+ * @returns transaction group object representing a claim rewards transaction
+ */
 export function prepareClaimRewardsTransactions(
   sender: string,
   suggestedParams: SuggestedParams,
