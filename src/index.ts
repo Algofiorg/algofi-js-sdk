2ced12a92e4e6e286872189410bc3fbf26633c41
jwtong <13jeremywt@gmail.com>
2022-03-28 20:39:44 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as v0 from "./v0"
 import * as v1 from "./v1"
 import * as v0test from "./v0test"
+import * as v1test from "./v1test"
 
-export { v0, v0test, v1 }
+export { v0, v0test, v1, v1test }
